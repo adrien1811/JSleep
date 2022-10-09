6bd334e919239cc49454262605fa8cb929b2863d
Adrien Ardra Ramadhan <adrien.ardra@ui.ac.id>
2022-10-09 22:51:45 +0700

    migrasi data ke intellij IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from intellij!!");
+        System.out.println("Hello from intellij!!!");
     }
 }
