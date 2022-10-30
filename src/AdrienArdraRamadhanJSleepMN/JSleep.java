9b0929c59560a2e609afa7bc8de88b7d37eaed2f
Adrien Ardra Ramadhan <adrien.ardra@ui.ac.id>
2022-10-30 23:38:30 +0700

    TP Modul 6

--- src/AdrienArdraRamadhanJSleepMN/JSleep.java
+++ src/AdrienArdraRamadhanJSleepMN/JSleep.java
@@ -1,11 +1,23 @@
 package AdrienArdraRamadhanJSleepMN;
 import java.sql.*;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 
 public class JSleep
 {
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String[] args) {
+
+        /**
         ArrayList<Room> RoomSerialized = new ArrayList<Room>();
         for (int i = 0; i < 5; i++) {
             RoomSerialized.add(i, JSleep.createRoom());
@@ -18,4 +30,19 @@ public class JSleep
                 Facility.WiFi, City.DEPOK, "JL. Margonda Raya");
         return room;
     }
-}
+         **/
+        String filepath= "C:\\Users\\ADRIEN\\OneDrive\\Kuliah smt 3\\OOP\\CS\\JSleep\\src\\lib\\city.json";
+        Gson gson = new Gson();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state -> System.out.println(state));
+            }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
