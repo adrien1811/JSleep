9533e86ac501b92b2f945da8423ed16cf05af129
Adrien Ardra Ramadhan <adrien.ardra@ui.ac.id>
2022-11-04 22:30:15 +0700

    PT Modul 6

--- src/AdrienArdraRamadhanJSleepMN/Account.java
+++ src/AdrienArdraRamadhanJSleepMN/Account.java
@@ -6,7 +6,7 @@ public class Account extends Serializable {
 
     public String name;
     public String email;
-    public static final String REGEX_EMAIL = "^[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-zA-Z]+$";
+    public static final String REGEX_EMAIL = "^[a-zA-Z0-9]+@{1}[a-zA-Z]+\\.{1}[.a-zA-Z]*[a-zA-Z]$";
     public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
     public String password;
     /** public Account(int id, String name, String email, String password){
--- src/AdrienArdraRamadhanJSleepMN/ThreadingObject.java
+++ src/AdrienArdraRamadhanJSleepMN/ThreadingObject.java
@@ -7,7 +7,7 @@ public class ThreadingObject extends Thread{
         this.start();
     }
     public void run(){
-        System.out.println(super.getName() + " is running");
-        System.out.println("Id Thread " + super.getId());
+        System.out.println(super.getName() + " is running "+ "\n");
+        System.out.println("Id Thread " + Thread.currentThread().getId() + "\n");
     }
-}
+    }
